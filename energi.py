e6371b8525b686cc184e04f2340cf6cd7818ff58
MarkusLokke <markuslokke@hotmail.com>
2026-02-25 14:02:38 +0100

    spotprisanalyse 2025

--- energi.py
+++ energi.py
@@ -2,21 +2,35 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+dager = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+m = ['00aar2025','01jan','02feb','03mar','04apr','05mai','06jun','07jul','08aug','09sep','10okt','11nov','12des']
+
+
 price = pd.read_csv('energi.csv',usecols=[3], names=['price'])
-pseries = price['price']
-pseries2 = np.zeros(int(len(pseries)/4))
-pseries3 = np.zeros(24)
+timepris_temp = price['price']
+timepris = np.zeros(int(len(timepris_temp)/4))
+timesnitt = np.zeros((13,24))
 
-for i in range(int(len(pseries2))):
-    pseries2[i] = np.mean((pseries[i*4],pseries[i*4+1],pseries[i*4+2],pseries[i*4+3]))
+for i in range(int(len(timepris))):
+    timepris[i] = np.mean((timepris_temp[i*4],timepris_temp[i*4+1],timepris_temp[i*4+2],timepris_temp[i*4+3]))
 
-for i in range(24):
-    pseries3[i] = np.mean(pseries2[i::24])
+"""for k in range(13):
+    for i in range(24):
+        timesnitt[k][i] = np.mean(timepris[i::24])"""
+
+for k in range(13):
+    a = int(np.sum(dager[:k])*24)
+    b = int(np.sum(dager[:(k+1)])*24+1)
+    if k==0:b=int(len(timepris)+1)
+    for i in range(24):
+        timesnitt[k][i] = np.mean(timepris[(i+a):b:24])
 
 t = np.linspace(0,23,24)
 
-print(np.mean(pseries2[23::24]))
+for i in range(13):
+    plt.plot(t,timesnitt[i])
+    plt.title(m[i])
+    plt.ylim(20,125)
+    plt.savefig(f'C:/Users/marku/Documents/batteri/figs/{m[i]}.png', format='png',dpi=200)
+    plt.clf()
 
-
-plt.plot(t,pseries3)
-plt.show()
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 import numpy as np
 a = np.linspace(0,23,24)
-print(a)
+
+print(a[0:24])
